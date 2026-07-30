7004923534221cfe314d05b204adac5c9c89dca0
Lincss777 <boss.lincss@ukr.net>
2026-07-30 18:31:11 +0300

    Initial project structure

---
+++ backend/app/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine   
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+def init_db():
+    Base.metadata.create_all(bind=engine)
